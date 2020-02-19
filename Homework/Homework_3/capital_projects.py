1b65357a0dd4ac621b311fdd90fb93328814d7e5
Trozenski <zachary.trozenski@ccac.org>
2020-02-19 18:47:16 -0500

    cleaning up some files and adding a ReadMe

--- Homework/Homework_3/capital_projects.py
+++ Homework/Homework_3/capital_projects.py
@@ -6,29 +6,54 @@ def open_csv_file():
     Return a dictionary containing the neighrborhoods
     """
     with open('capital_projects.csv') as dataset:
+        
+        
         data = csv.DictReader(dataset)
-        data_dict = {'total_budget': 0, 'total_neighborhoods': 0}
+        data_dict = {'total_budget': 0, 'total_neighborhoods': 0, 'Manchester': 0, 'Squirrel_Hill':0}
+        
+        
         total_cost = 0
+        manchester_projects = 0
+        squirrel_hill_projects = 0
         neighborhoods = []
+        
+        
         for row in data:
             total_cost += int(row['budgeted_amount'])
             if row['neighborhood'] not in neighborhoods:
                 neighborhoods.append(row['neighborhood'])
+            if row['neighborhood'] == 'Manchester':
+                manchester_projects += 1
+            if row['neighborhood'] == 'Squirrel Hill North' or row['neighborhood'] == 'Squirrel Hill South':
+                squirrel_hill_projects += 1
+        
+        
         data_dict['total_budget'] = total_cost
         total_num = len(neighborhoods)
         data_dict['total_neighborhoods'] = total_num
+        data_dict['Manchester'] = manchester_projects
+        data_dict['Squirrel_Hill'] = squirrel_hill_projects
+        
         
     return data_dict
 
 def basic_stats(dictionary):
     money = dictionary['total_budget']
     places = dictionary['total_neighborhoods']
+    manchester = dictionary['Manchester']
+    squill = dictionary['Squirrel_Hill']
     
     average = money / places
-
-    print("In the city of Pittsburgh, there are", places, "neighborhoods.") 
+    
+    print()
+    print("In the city of Pittsburgh, there are", places, "neighborhoods represented in the capital projects data set.") 
     print("From 2017 - 2019 the city has dedicated approximately {:9,.2f} dollars to capital projects.".format(money))
     print("This equates to roughly {:7,.2f} dollars per neighborhood.".format(average))
+    print()
+    print("The number of projects completed or running in the Manchester neighborhood on the North Shore are", manchester, ".")
+    print("The number of projects completed or running in both sections of Squirrel Hill are", squill, ".")
+    print()
+    
 
 capital_projects = open_csv_file()
 basic_stats(capital_projects)
---
+++ In-class/Week_4/README.md
@@ -0,0 +1,7 @@
+# Week 4
+
+Topics covered in Week 4:
+
+* Git: remote branches, tracking branches, fetches, pulls, pushes, and pull requests
+* JSON and Python
+
