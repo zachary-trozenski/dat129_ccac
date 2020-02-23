9deb7f21793398001634b961dd21b5b271b84923
Zach T <Zach@MacBook-Air.local>
2020-02-23 16:25:01 -0500

    Updating file structure to creat new folders for homework for week 4; adding homework files in separate folder with output and input files; separated mini tasksfrom homework

--- Homework/Homework_4/mini_task_1_2.py
+++ Homework/Homework_4/mini_task_1_2.py
@@ -3,39 +3,9 @@
 Created on Wed Feb 19 18:57:59 2020
 
 @author: zachary.trozenski
-"""
 
-# pull in the json & csv module into the script
-import json
-import csv
-from pprint import pprint
-
-# open the file gitinfo.json and using the load() method, assign the json
-# file to the variable gitkeywords
-with open('gitinfo.json') as jsontest:
-    gitkeywords = json.load(jsontest)
-    print(gitkeywords)
-
-# access the dictionary key Remote within the json object and pull the 2nd list element 
-print()
-pprint(gitkeywords['Remote'][1])
-print()
-
-# encode python object as json and write to a file
-projects = {}
-projects["group1"] = ['Dan', 'Eric1', 'Eric2', 'Eric3']
-projects["group2"] = ('Zach', 'Taichi', 'Joel')
-projects['unallocated'] = 452
-pprint(projects)
-
-# use the dump() method to create a json file from the python object
-with open('groups.json', 'w') as groupfile:
-    json.dump(projects, groupfile)
-    
-# dump() syntax (object_to_dump, file_to_dump_into)
-    
 # Mini task #1 & #2
-   
+"""   
 """
 For Mini Task #2:
 The plan here is to open the capital projects file as a dictionary and assign
@@ -78,12 +48,3 @@ with open('capital_projects.csv') as projects:
                 area_list.append(rows['area'])
     for things in area_list:
         print(things)
-
-    
-            
-"""
-Allow the user to specify search criteria for project fiscal year, start date, area, asset_type, and planning status
-"""
-        
-
-
