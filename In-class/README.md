c481822b763bf8e8c49cf41e86e7d4935b090ad1
Zach T <Zach@MacBook-Air.local>
2020-03-31 20:45:20 -0400

    Adding more fleshed out web scraping script and README

---
+++ In-class/README.md
@@ -0,0 +1,17 @@
+# Webscraping project
+## Details
+I'm using [IMDB's Top 250 film list](https://www.imdb.com/chart/top/?ref_=nv_mv_250)
+
+## Research question:
+The 1970's is often lauded as producing some of the most critically
+acclaimed films in cinematic history. However, how many of IMDB's Top
+250 films are actually from the 70's and what is the proportion of the whole?
+If not the seventies, then which decade is the most "critically acclaimed"?
+
+## Additional output:
+Proportions each decade in percentages, in ascending order from 1920's to 2010's
+
+### Additional work
+* Convert lists to json object
+* Dump output into a CSV or JSON
+* Pull title along with year
--- In-class/Week_8/web_scraper.py
+++ In-class/Week_8/web_scraper.py
@@ -1,5 +1,4 @@
 """
-Scraping demo, following along in class.
 Before scraping check:
     1 - Are the URLs encoded sensibly
     such that we can use the urlib to grab
@@ -8,11 +7,15 @@ Before scraping check:
     scrape enclosed in HTML tags with distinct 
     names or tag names?
 
-My experiment:
+Site i'm using to scrape:
 https://www.imdb.com/chart/top/?ref_=nv_mv_250
 
-What proportion of the Top Rated movies are from
-the 1970s?
+Research question:
+The 1970's is often lauded as producing some of the most critically
+acclaimed films in cinematic history. However, how many of IMDB's Top
+250 films are actually from the 70's and what is the proportion of the whole?
+If not the seventies, then which decade is the most "critically acclaimed"?
+
 """
 
 import urllib.request
@@ -33,33 +36,107 @@ def get_html_text(url):
 
 imdb_html = get_html_text(url)
 
-#print just to check it works
-#print(imdb_html)
-
 # Pass raw HTML and desired parser to beautiful soup
 soup = BeautifulSoup(imdb_html, 'html.parser')
 
+## Initialize some lists to store figures pulled from the html data
+# This list will house raw extracted movie years
 movie_years = []
+# This list will house the cleaned years sliced from movie_years
+clean_years = []
 
-# with html passed to BeautifulSoup we can extract components
-# 2ith spcific calls to find or find all
+# These lists will house each year counted in the list of 250 for the decade
+# in which they were released
+twenties_films = []
+thirties_films = []
+forties_films = []
+fifties_films = []
+sixties_films = []
+seventies_films = []
+eighties_films = []
+nineties_films = []
+aughts_films = []
+teens_films = []
+
+# initialize a list of all sorted release years to iterate over later
+all_films = [
+twenties_films,
+thirties_films,
+forties_films,
+fifties_films,
+sixties_films,
+seventies_films,
+eighties_films,
+nineties_films,
+aughts_films,
+teens_films]
+
+# locate all instances of the <td> tag in the html
+# and pull the title column fields from within that tag
 movie_titles = soup.find_all('td', 'titleColumn')
 
-# for movies in movie_titles:
-#     year = movies.find('span', 'secondaryInfo')
-#     movie_years.append(movies)
-
+# iterate of the html lines extracted by pulling out the release years
+# in string format and add it to the movie_years list
 for movies in movie_titles:
     year = movies.find('span').string
     movie_years.append(year)
-    #print(year)
 
-print(movie_years)
+# iterate over each of the raw release years in the movie_years list
+# and slice the list elements so only the digits are extracted
+for films in movie_years:
+    clean_years.append(films[1:5])
 
-#print(movie_titles)
+# iterate over the list of cleaned release years in clean_years and
+# sort them into lists representing each decade from 1920 to 2019
+for flicks in clean_years:
+    if int(flicks) in range(1970,1980):
+        seventies_films.append(flicks)
+    if int(flicks) in range(1920,1930):
+        twenties_films.append(flicks)
+    if int(flicks) in range(1930,1940):
+        thirties_films.append(flicks)
+    if int(flicks) in range(1940,1950):
+        forties_films.append(flicks)
+    if int(flicks) in range(1950,1960):
+        fifties_films.append(flicks)
+    if int(flicks) in range(1960,1970):
+        sixties_films.append(flicks)
+    if int(flicks) in range(1980,1990):
+        eighties_films.append(flicks)
+    if int(flicks) in range(1990,2000):
+        nineties_films.append(flicks)
+    if int(flicks) in range(2000,2010):
+        aughts_films.append(flicks)
+    if int(flicks) in range(2010,2020):
+        teens_films.append(flicks)
 
-#total_films = 250
-#seventies_films = []
+# intialize some variables to be used for calculations
+total_films = 250
+proportions = []
+
+# calculate the proportions for each decade and store them in a list
+for entries in all_films:
+    proportions.append((len(entries)/total_films) * 100)
+
+print()
+print("Out of the Top 250 films from IMDB,", len(seventies_films), "are from the 1970's.")
+print("Percentage of Top 250 films from the 1970's: {:2.2}%".format(proportions[int(5)]))
+print()
+print("The two decades with the most entires in IMDB's Top 250 are the early 2000's" +
+    " and the 2010's with", len(aughts_films), "films a piece.")
+print("The proportions of films from each decade (in ascending order) are: ")
+
+for percents in proportions:
+    print("{:.2f}%".format(percents))
+
+
+###
+# SCRATCH
+###
+
+# check to make sure all 250 are accounted for
+# for entries in all_films:
+#     print(len(entries))
 
 # movie titles is an iterable and contains all the mini-trees
 # who's parent is an <a> tag of class title
@@ -73,16 +150,8 @@ print(movie_years)
 # proportion = len(seventies_films) / total_films
 # print statment with the proportion
 
+# for movies in movie_titles:
+#     year = movies.find('span', 'secondaryInfo')
+#     movie_years.append(movies)
+
 # i'd also like to print the titles that were released in the 70s
-
-
-
-
-
-
-
-
-
-
-
-
